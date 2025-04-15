c4c8f0aad4eb51f05a8b83e72dd2d49a6572a902
Galih066 <galihdapaimanda@gmail.com>
2025-04-15 20:37:15 +0800

    Add reactivity state and effect

--- src/components/DisplayName.svelte
+++ src/components/DisplayName.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
-  const name = "Svelte";
-  const src = "https://picsum.photos/200/300";
-  const alt = "Svelte logo";
+    const name = "Svelte";
+    const src = "https://picsum.photos/200/300";
+    const alt = "Svelte logo";
+    let firstName = $state('Galeh');
+    let lastName = $state('Imanda');
+    const fullName = $derived.by(() => {
+        return `${firstName} ${lastName}`;
+    });
+    let defaultName = $state('Galeh Imanda');
 </script>
 
-<img {src} {alt}/>
-<h1>Welcome to {name}!</h1>
+<!-- <img {src} {alt}/> -->
+<input type="text" bind:value={firstName} />
+<input type="text" bind:value={lastName} />
+<input type="text" bind:value={defaultName} />
+<h1>My fullname is {fullName || defaultName}!</h1>
