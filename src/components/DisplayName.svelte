43be160e22fd488d94d353df3d14dbdea65beeae
Galih066 <galihdapaimanda@gmail.com>
2025-04-15 20:19:15 +0800

    Basic component

---
+++ src/components/DisplayName.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  const name = "Svelte";
+  const src = "https://picsum.photos/200/300";
+  const alt = "Svelte logo";
+</script>
+
+<img {src} {alt}/>
+<h1>Welcome to {name}!</h1>
