799182a4501f77de8d361266ef93c2c4e425192d
Galih066 <galihdapaimanda@gmail.com>
2025-04-17 14:20:02 +0800

    Basic effect

---
+++ src/components/RandomNumber.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let randomNumber = $state(Math.floor(Math.random() * 10));
+    let doubleIt = $state(0);
+
+    $effect(() => {
+        doubleIt = randomNumber * 2
+    })
+</script>
+
+<h2>Random Number: {randomNumber}</h2>
+<h2>Double Randomed Number: {doubleIt}</h2>
+<button onclick={() => {
+    randomNumber = Math.floor(Math.random() * 10);
+    console.log(randomNumber);
+}}>Click Me</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import DisplayName from "../components/DisplayName.svelte";
+    import RandomNumber from "../components/RandomNumber.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <DisplayName />
+<RandomNumber />
