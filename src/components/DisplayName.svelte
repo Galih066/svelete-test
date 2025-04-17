163712bc790e4cda8e2db81cf9f862227b776041
Galih066 <galihdapaimanda@gmail.com>
2025-04-17 13:58:28 +0800

    More about directive

--- src/components/DisplayName.svelte
+++ src/components/DisplayName.svelte
@@ -2,17 +2,30 @@
     const name = "Svelte";
     const src = "https://picsum.photos/200/300";
     const alt = "Svelte logo";
-    let firstName = $state('');
-    let lastName = $state('');
+    let firstName = $state('Test');
+    let lastName = $state('Ing');
     const fullName = $derived.by(() => {
         console.log('Fullname derived')
         return `${firstName} ${lastName}`;
     });
     let defaultName = $state('');
+
+    $effect(() => {
+        if (defaultName || fullName) {
+            console.log('User has default name of fullname')
+        }
+    })
 </script>
 
 <!-- <img {src} {alt}/> -->
-<input type="text" bind:value={firstName} />
+<input
+    type="text"
+    value={firstName}
+    oninput={event => {
+        firstName = event.currentTarget.value;
+        console.log(fullName)
+    }}
+/>
 <input type="text" bind:value={lastName} />
 <input type="text" bind:value={defaultName} />
 <h1>My fullname is {fullName || defaultName}!</h1>
